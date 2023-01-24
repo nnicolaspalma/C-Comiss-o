56ed3960b016783f74355866595d8a7316b6d32b
Nicolas Palma <61471877+nnicolaspalma@users.noreply.github.com>
2023-01-24 13:57:07 -0300

    Sistema De ParqueDiversão

---
+++ ParqueDiversão/ParqueDiversão/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ParqueDiversao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            Console.WriteLine("Qual a sua altura?");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            double altura = 1.5;
+            if (n1 >= altura) {
+                Console.WriteLine("Podera ir ao brinquedo");
+             } else {
+                Console.WriteLine("Não podera ir ao brinquedo");
+            }
+        }           
+    }
+}
