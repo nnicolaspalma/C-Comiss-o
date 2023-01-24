25b3d62f932ff9aaf0a6aa2b49ccd7f0e483a4ce
Nicolas Palma <61471877+nnicolaspalma@users.noreply.github.com>
2023-01-24 13:52:08 -0300

    Sistema De Número Intero_ou_Par

---
+++ InteiroOuPar/InteiroOuPar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace numero_par_ou_impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("\n\tDigite um número inteiro:");
+            x = int.Parse(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("\nO número é par");
+            }
+            else
+            {
+                Console.WriteLine("\nO número é impar");
+            }
+        }
+    }
+}
