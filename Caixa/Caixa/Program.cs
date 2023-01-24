8c489647b03653c3defd46253f43d6dfcf6c0191
Nicolas Palma <61471877+nnicolaspalma@users.noreply.github.com>
2023-01-24 13:36:14 -0300

    Sistema de Caixa

---
+++ Caixa/Caixa/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Caixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o valor do produto 1");
+            double produto1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor do produto 2");
+            double produto2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor do desconto em %");
+            double valorDescontoEmPorcentagem = double.Parse(Console.ReadLine());
+
+            double valorSemDesconto = produto1 + produto2;
+
+            double valorDesconto = valorSemDesconto * valorDescontoEmPorcentagem / 100;
+
+            double valorComDesconto = valorSemDesconto - valorDesconto;
+
+            Console.WriteLine(String.Format("Valor com desconto {0}, valor do desconto {1}", valorComDesconto, valorDesconto));
+        }
+    }
+}
