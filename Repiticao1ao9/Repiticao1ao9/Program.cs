89e379e49aebc08e0a5950d3838eb1bde33f98fa
Nicolas Palma <61471877+nnicolaspalma@users.noreply.github.com>
2023-01-24 14:01:25 -0300

    Sistema De Repiticao1ao9

---
+++ Repiticao1ao9/Repiticao1ao9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Repeticao1ao9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sequencias = 8;
+            int sequenciaMaxima = 9;
+
+            for (int contadorSequencias = 1; contadorSequencias <= sequencias; contadorSequencias++)
+            {
+                string sequencia = "";
+                for (int contadorSequencia = 1; contadorSequencia <= sequenciaMaxima; contadorSequencia++)
+                {
+                    sequencia += Convert.ToString(contadorSequencia);
+                }
+
+                Console.Write(sequencia);
+            }
+        }
+    }
+}
