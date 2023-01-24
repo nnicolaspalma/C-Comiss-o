ed20a1a6730a7fa4aa7ac6d54deb1ccf578a00fa
Nicolas Palma <61471877+nnicolaspalma@users.noreply.github.com>
2023-01-24 13:56:18 -0300

    Sistema de PararNumeroInformado0

---
+++ PararNumeroInformado0/PararNumeroInformado0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PararNumeroInformado0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double somaTotal = 0;
+
+            Console.WriteLine("Informe um numero para somar");
+            double numeroInformado = Double.Parse(Console.ReadLine());
+
+            while (numeroInformado != 0)
+            {
+                somaTotal += numeroInformado;
+
+                Console.WriteLine("Informe mais um numero para somar");
+                numeroInformado = Double.Parse(Console.ReadLine());
+
+                if (numeroInformado == 0)
+                {
+                    Console.WriteLine("A soma total é: " + somaTotal);
+                }
+            }
+        }
+    }
+}
