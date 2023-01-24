4debad4f7aba718caf0518d09b34e23a33aae8eb
Nicolas Palma <61471877+nnicolaspalma@users.noreply.github.com>
2023-01-24 13:53:44 -0300

    Sistema de Média Aluno

---
+++ MédiaAluno/MédiaAluno/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MédiaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, media = 0;
+
+            // exibiir no console
+            Console.Write("Entre com a nota 1 : ");
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Entre com a nota 2 : ");
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Entre com a nota 3 : ");
+            nota1 = double.Parse(Console.ReadLine());
+
+            // processamento
+            media = (nota1 + nota2 + nota3) / 3;
+
+            // saida
+            Console.Write("A media ponderada é: " + media);
+
+            // espera entrada do teclado para encerrar
+            Console.ReadKey();
+        }
+
+    }
+}
