77e2e89adf7ad7990016470edae4558d711e2676
Nicolas Palma <61471877+nnicolaspalma@users.noreply.github.com>
2023-01-24 13:34:58 -0300

    Sistema De Comissão

---
+++ Comissão/Comissão/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Comissão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o valor do produto 1");
+            double produto1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor do produto 2");
+            double produto2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor da comissão em %");
+            double valorcomissão = double.Parse(Console.ReadLine());
+
+            double valortotal = produto1 + produto2;
+
+            double valorganho = valortotal * valorcomissão / 100;
+
+            double valordesconto = valortotal;
+
+            Console.WriteLine(String.Format("Valor total {0}, valor ganho na comissão {1}", valordesconto, valorganho));
+        }
+    }
+}
