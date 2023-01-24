ca87bcd5332565a79e422de2a51c76baac8878f2
Nicolas Palma <61471877+nnicolaspalma@users.noreply.github.com>
2023-01-24 14:09:35 -0300

    Sistema De Salario

---
+++ Salario/Salario/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valor atual do salario");
+            double salarioAtual = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o aumento em %");
+            double aumentoEmPorcentagem = double.Parse(Console.ReadLine());
+
+            double aumentoSalario = salarioAtual * aumentoEmPorcentagem / 100;
+
+            double novoSalario = aumentoSalario + salarioAtual;
+
+            Console.WriteLine(String.Format("Salario atual {0}, novo salario {1}", salarioAtual, novoSalario));
+        }
+    }
+}
