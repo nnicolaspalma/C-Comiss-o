d066043233404478e9be57affc20abe9a0b890b4
Nicolas Palma <61471877+nnicolaspalma@users.noreply.github.com>
2023-01-24 13:55:19 -0300

    Sistema de MultiplosDe4De0a1000

---
+++ MultiplosDe4De0a1000/MultiplosDe4De0a1000/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MultiplosDe4De0a1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = 1000;
+            int multiple = 4;
+
+            Console.WriteLine("Multiplos de 4 de 0 até 1000");
+
+            for (int count = 0; count <= numbers; count++)
+            {
+                bool isMultiple = (count % multiple) == 0;
+
+                if (isMultiple)
+                {
+                    Console.WriteLine(count);
+                }
+            }
+        }
+    }
+}
